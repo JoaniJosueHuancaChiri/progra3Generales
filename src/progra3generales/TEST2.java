94b1d45ff0b993558597289a39967a9eb729ed18
JoaniJosueHuancaChiri <joanihuanca@gmail.com>
2026-03-14 21:39:08 -0400

    test reporte 1

---
+++ src/progra3generales/TEST2.java
@@ -0,0 +1,14 @@
+package progra3generales;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Sony Vaio
+ */
+public class TEST2 {
+    
+}
